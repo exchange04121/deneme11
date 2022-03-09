a59525378fada932e04b186bc0bfc205b5936eb6
osoka <osman.bozdag@yahoo.com>
2022-03-09 21:17:25 +0100

    ekleme3

--- deneme11/src/Main.java
+++ deneme11/src/Main.java
@@ -2,6 +2,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("ekleme");
         System.out.println("ekleme2");
+        System.out.println("ekleme3");
 
     }
 }
